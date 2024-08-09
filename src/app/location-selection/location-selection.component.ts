895646331bf58a6fa6db116f38caf4f947605c3a
Kumar MA <public.kumar@gmail.com>
2024-08-08 17:31:36 -0700

    add map with auto pinned

--- src/app/location-selection/location-selection.component.ts
+++ src/app/location-selection/location-selection.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
 import { Router } from '@angular/router';
+
+declare var google: any;
 
 @Component({
   selector: 'app-location-selection',
@@ -9,8 +11,9 @@ import { Router } from '@angular/router';
 export class LocationSelectionComponent implements OnInit {
   locations: any[] = [];
   origin: string = '';
+  map!: google.maps.Map;
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private ngZone: NgZone) {
     const navigation = this.router.getCurrentNavigation();
     if (navigation?.extras.state) {
       this.locations = navigation.extras.state['stores'];
@@ -21,6 +24,64 @@ export class LocationSelectionComponent implements OnInit {
   ngOnInit(): void {
     console.log('Locations:', this.locations);
     console.log('Origin:', this.origin);
+    this.loadGoogleMapsScript().then(() => {
+      this.initMap();
+      this.addMarkers();
+    });
+  }
+
+  loadGoogleMapsScript(): Promise<void> {
+    return new Promise((resolve, reject) => {
+      if (typeof google !== 'undefined') {
+        resolve();
+        return;
+      }
+
+      const script = document.createElement('script');
+      script.src = `https://maps.googleapis.com/maps/api/js?key=YOUR_GOOGLE_MAPS_API_KEY&libraries=places`;
+      script.async = true;
+      script.defer = true;
+      script.onload = () => resolve();
+      script.onerror = (error) => reject(error);
+      document.head.appendChild(script);
+    });
+  }
+
+  initMap() {
+    const mapElement = document.getElementById('map') as HTMLElement;
+
+    this.map = new google.maps.Map(mapElement, {
+      center: { lat: 34.0522, lng: -118.2437 }, // Default to Los Angeles, or customize
+      zoom: 10,
+      mapTypeControl: false
+    });
+  }
+
+  addMarkers() {
+    this.locations.forEach(location => {
+      const geocoder = new google.maps.Geocoder();
+      geocoder.geocode({ address: location.address }, (results: any, status: any) => {
+        if (status === google.maps.GeocoderStatus.OK) {
+          const marker = new google.maps.Marker({
+            map: this.map,
+            position: results[0].geometry.location,
+            title: location.storeName
+          });
+
+          const infoWindow = new google.maps.InfoWindow({
+            content: `<h4>${location.storeName}</h4><p>${location.address}</p><p>${location.phoneNumber}</p><p>Distance: ${location.distance}</p><p>Duration: ${location.duration}</p>`
+          });
+
+          marker.addListener('click', () => {
+            this.ngZone.run(() => {
+              infoWindow.open(this.map, marker);
+            });
+          });
+        } else {
+          console.error('Geocode failed: ' + status);
+        }
+      });
+    });
   }
 
   selectLocation(location: any): void {
