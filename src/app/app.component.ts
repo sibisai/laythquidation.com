66c67b992572cc64556c098aab69eedc811fe8b5
Kumar MA <public.kumar@gmail.com>
2024-08-20 15:24:05 -0700

    transparent header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  tripInfo: any;
+export class AppComponent implements OnInit {
+  isLocationSelectionPage: boolean = false;
 
-  handleTripGenerated(event: any) {
-    this.tripInfo = event;
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        // Check if the current URL includes the location selection path
+        this.isLocationSelectionPage = event.urlAfterRedirects.includes('/select-locations');
+      }
+    });
   }
 }
