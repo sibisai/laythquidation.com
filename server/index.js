17e5cce782712ca37b14e66c4e78e4694b1c23bc
Kumar MA <public.kumar@gmail.com>
2024-08-20 18:25:08 -0700

    update prod env

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ app.use(express.static(path.join(__dirname, '../dist/trip-planner/browser')));
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, '../dist/trip-planner/browser/index.html'));
 });
-
+  /*
 // local testing config
 const googleMapsApiKey = process.env.GOOGLE_MAPS_API_KEY || 'AIzaSyCVMfV8HMmQHWcgZfF1ry3PCQXSxVtwOeg';
 console.log('maps api key', googleMapsApiKey);
@@ -31,7 +31,7 @@ const pool = new Pool({
   console.log('database', process.env.DATABASE_URL || 'postgres://sibi:leo@localhost:5432/maclocations');
    
 
-  /*
+*/
   // Prod config
   if (!process.env.GOOGLE_MAPS_API_KEY || !process.env.DATABASE_URL) {
     throw new Error('Critical environment variables are missing!');
@@ -45,7 +45,7 @@ const pool = new Pool({
     connectionString: process.env.DATABASE_URL || 'postgres://sibi:leo@localhost:5432/maclocations',
     ssl: process.env.DATABASE_URL.includes('localhost') ? false : { rejectUnauthorized: false } // Disable SSL for local, enable for production
   });
- */
+ 
 const geocodeCache = new NodeCache({ stdTTL: 2592000, checkperiod: 3600 }); // Cache for 30 days
 const geocodeAddress = async (address) => {
   try {
