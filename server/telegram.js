d76ba1f8eab26ca5bf745fb722ed0c0efdc8d727
Kumar MA <public.kumar@gmail.com>
2024-08-22 16:28:43 -0700

    update telegram chat id

--- server/telegram.js
+++ server/telegram.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 const telegramBotToken = process.env.TELEGRAM_BOT_TOKEN || '7308283785:AAGqqRxvPr3m1wqZtGFOnG_rcQGdqXkkuJI';
 // Production channel
-const telegramChatId = process.env.TELEGRAM_CHAT_ID || '-4515672907';
+const telegramChatId = process.env.TELEGRAM_CHAT_ID || '-1002201783428';
 
 // Dev Test Channel
 // const telegramChatId = process.env.TELEGRAM_CHAT_ID || '-4529142676';
