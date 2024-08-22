fc51120106022718dd221e6ae53f01d52e436510
Kumar MA <public.kumar@gmail.com>
2024-08-21 19:20:22 -0700

    broke but width is maxed

--- src/app/services/trip-planner.service.ts
+++ src/app/services/trip-planner.service.ts
@@ -6,8 +6,8 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class TripPlannerService {
-  // private apiUrl = 'http://localhost:5001';
-  private apiUrl = 'https://route-plug-9cc83536ca43.herokuapp.com';
+  private apiUrl = 'http://localhost:5001';
+  // private apiUrl = 'https://route-plug-9cc83536ca43.herokuapp.com';
 
   constructor(private http: HttpClient) {}
 
