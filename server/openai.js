718dd17107997bab89c813dc41b69aa10a3900ad
sibisai <110426373+sibisai@users.noreply.github.com>
2025-04-30 14:27:49 -0700

    Update openai.js

--- server/openai.js
+++ server/openai.js
@@ -3,7 +3,6 @@ const OpenAI = require('openai');
 
 
 const openai = new OpenAI({
-  // apiKey: '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'
   apiKey: process.env.OPENAI_API_KEY
 });
 
